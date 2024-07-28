da5d9da81f761ca86e2ebfff83484b5f2cdc61dd
Yash-6503 <yashwalke6503@gmail.com>
2024-07-28 12:34:54 +0530

    setup & initial commit

---
+++ food-ordering/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+export default function Home() {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
